a0d5c9790d212dd73d9dcdfac2316610fb74faf7
alencshaji <alencshaji@gmail.com>
2023-10-21 10:20:52 +0530

    changed ts by subscribe

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -10,14 +10,18 @@ export class LandingComponent {
 
   movieTitle: string = '';
   movieData: any = null;
-  constructor(private http: HttpClient) {}
-  async search() {
+  constructor(private http: HttpClient) { }
+  search() {
     if (this.movieTitle) {
       try {
         const apiKey = 'e57c2b8c';
         const url = `https://www.omdbapi.com/?t=${encodeURIComponent(this.movieTitle)}&apikey=${apiKey}`;
-        const data = await this.http.get(url).toPromise();
-        this.movieData = data;
+        const data = this.http.get(url).subscribe({
+          next: (data: any) => {
+            this.movieData = data;
+          }
+        });
+
       } catch (error) {
         console.error('Error fetching data:', error);
       }
